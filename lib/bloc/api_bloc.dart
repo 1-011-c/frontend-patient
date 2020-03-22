71ae25e8572cce1f41a0d2040969f2fd7f725da7
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-03-22 13:08:22 +0100

    Persist TestCase on API Get

--- lib/bloc/api_bloc.dart
+++ lib/bloc/api_bloc.dart
@@ -4,12 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend_patient/event/api_event.dart';
 import 'package:frontend_patient/model/corona_response.dart';
 import 'package:frontend_patient/service/api_service.dart';
+import 'package:frontend_patient/service/storage_service.dart';
 import 'package:frontend_patient/state/api_state.dart';
 
 class APIBloc extends Bloc<APIEvent, APIState> {
-
-  static const API_ERROR_TIME = 2;
-
   @override
   Stream<APIState> mapEventToState(APIEvent event) async* {
     if (event is GetAPIEvent) {
@@ -17,10 +15,13 @@ class APIBloc extends Bloc<APIEvent, APIState> {
 
       final CoronaResponse apiResponse = await APIService.get(event.url);
 
-      // Persist Testcase
-
       if (apiResponse.coronaTestCase != null) {
-        yield APILoaded(response: apiResponse.coronaTestCase);
+        if(await StorageService.storeOrUpdate(apiResponse.coronaTestCase)) {
+          yield APILoaded(response: apiResponse.coronaTestCase);
+        }
+        else {
+          yield APIError(message: 'Could not store test-case');
+        }
       }
       else {
         yield APIError(message: apiResponse.errorMessage);
